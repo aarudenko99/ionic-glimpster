9d807f62ff276999ae61728349e24d2008554f74
aarudenko99 <aarudenko99@gmail.com>
2020-04-27 10:54:45 +0300

    Add landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { LandingPage } from './landing/landing.page';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'landing',
+    loadChildren: () => import('./landing/landing.module').then(m => m.LandingPageModule)
   }
 ];
 @NgModule({
