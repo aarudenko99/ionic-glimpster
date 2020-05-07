f3b629b5685d1efb3890e908f24f9eb5344c7429
Alexander Rudenko <aarudenko99@gmail.com>
2020-05-07 03:45:31 +0300

    Fix feature of sending data of contest

--- src/app/all-contests/all-contests.page.ts
+++ src/app/all-contests/all-contests.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 import { LoadingController, ToastController } from '@ionic/angular';
 import { Storage } from '@ionic/storage';
@@ -13,64 +13,26 @@ import { AllService } from '../all.service';
 export class AllContestsPage implements OnInit {
   loading: any;
   imageBaseUrl = "";
-  comingImage = "";
-  comingUser = "";
-  comingTitle = "";
-  comingDateTime = "";
-
   placeholderImage = "../../assets/imgs/placeholder.png";
 
   pastContest = [];
+  upcomingContest = [];
 
   constructor(
     public allService: AllService,
     public loadingCtrl: LoadingController,
     public router: Router,
     public storage: Storage,
-    public toastController: ToastController
+    public toastController: ToastController,
+    public route: ActivatedRoute
   ) { }
 
   ngOnInit() {
-    this.showLoader();
-    this.imageBaseUrl = this.allService.getImageBaseUrl();
-    this.allService.getAllContest().subscribe(
-      data => {
-        if(data['success'] == 1) {
-          this.dismissLoading();
-          this.comingImage = this.imageBaseUrl + data['upcoming_contest'][0]['media'];
-          this.comingUser = data['upcoming_contest'][0]['username'];
-          this.comingTitle = data['upcoming_contest'][0]['title'];
-          this.comingDateTime = data['upcoming_contest'][0]['start_date'] + " at " + data['upcoming_contest'][0]['start_time'] + " (GMT)";
-
-          this.pastContest = data['passed_contest'];
-          this.dismissLoading();
-        }
-        
-      }
-    )
-  }
-
-  async showLoader(){
-    this.loading = await this.loadingCtrl.create({
-      message: 'please wait',
-      backdropDismiss: true,
+    this.route.queryParams.subscribe(params => {
+      this.imageBaseUrl = this.router.getCurrentNavigation().extras.state.imageBaseUrl;
+      this.pastContest = this.router.getCurrentNavigation().extras.state.pastContest;
+      this.upcomingContest = this.router.getCurrentNavigation().extras.state.upcomingContest;
     });
-    
-    this.loading.present();
-    await this.loading.onDidDismiss();
-  }
 
-  async dismissLoading() {
-    await this.loading.dismiss();
   }
-
-  async presentToast(text) {
-    const toast = await this.toastController.create({
-      message: text,
-      position: 'bottom',
-      duration: 3000
-    });
-    toast.present();
-  }
-
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';
+
+import { LoadingController, ToastController } from '@ionic/angular';
+import { Storage } from '@ionic/storage';
+import { AllService } from '../all.service';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +11,68 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  loading: any;
+  imageBaseUrl = "";
+  comingImage = "";
+  pastContest = [];
+  upcomingContest = [];
+  navigationExtras: NavigationExtras;
 
-  constructor() {}
+  constructor(
+    private allService: AllService,
+    public loadingCtrl: LoadingController,
+    private storage: Storage,
+    private router: Router,
+    private toastController: ToastController
+  ) {}
+
+  ngOnInit() {
+    this.imageBaseUrl = this.allService.getImageBaseUrl();
+    this.allService.getAllContest().subscribe(
+      data => {
+        if(data['success'] == 1) {
+          this.upcomingContest = data['upcoming_contest']
+          this.pastContest = data['passed_contest'];
+          this.navigationExtras = {
+            state: {
+              upcomingContest: this.upcomingContest,
+              pastContest: this.pastContest,
+              imageBaseUrl: this.imageBaseUrl
+            }
+          };
+        }
+        else {
+          this.presentToast("Error");
+        }
+      }
+    )
+  }
+
+  viewAll() {
+    this.router.navigate(['/all-contests'], this.navigationExtras);
+  }
+
+  async showLoader(){
+    this.loading = await this.loadingCtrl.create({
+      message: 'please wait',
+      backdropDismiss: true,
+    });
+    
+    this.loading.present();
+    await this.loading.onDidDismiss();
+  }
+
+  async dismissLoading() {
+    await this.loading.dismiss();
+  }
+
+  async presentToast(text) {
+    const toast = await this.toastController.create({
+      message: text,
+      position: 'bottom',
+      duration: 3000
+    });
+    toast.present();
+  }
 
 }
