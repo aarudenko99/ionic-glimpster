b285df33551269c2d0bfc369c00ce17ab2588c5a
Alexander Rudenko <aarudenko99@gmail.com>
2020-05-15 21:51:47 +0300

    Add create post feature

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { } from '@angular/forms';
-
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 import { Storage } from '@ionic/storage';
 
 import { AllService } from '../all.service';
@@ -12,15 +12,35 @@ import { AllService } from '../all.service';
 })
 export class Tab3Page {
   body = new FormData();
+  postText = "";
 
   constructor(
     private allService: AllService,
-    private storage: Storage
+    private storage: Storage,
+    private toastController: ToastController,
+    private router: Router
   ) {}
 
   create_post() {
+    if(this.postText == "") {
+      this.presentToast("Please enter post text");
+      return;
+    }
     this.storage.get('user').then(userInfo => {
-      console.log(userInfo);
+      // console.log(this.postText, userInfo.user_id);
+      this.body.append('user_id', userInfo.user_id);
+      this.body.append('post_text', this.postText);
+      this.allService.createPost(this.body).subscribe(
+        data => {
+          if(data['success'] == 1) {
+            this.presentToast(data['message']);
+            this.router.navigate(['/tabs/tab1']).then(() => {
+              window.location.reload();
+            });
+            // console.log(data['message']);
+          }
+        }
+      )
       // this.body.append('user_id', userInfo.user_id);
       // this.body.append('post_text', );
       // this.body.append('ad_type', userInfo['user_info']);
@@ -30,5 +50,14 @@ export class Tab3Page {
       // this.body.append('state', );
       // this.body.append('country', );
     });
+  }
+
+  async presentToast(text) {
+    const toast = await this.toastController.create({
+      message: text,
+      position: 'bottom',
+      duration: 3000
+    });
+    toast.present();
   }
 }
