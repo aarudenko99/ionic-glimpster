268b3b59caa581f4d6f7cbc9c48f32a51870702d
aarudenko99 <aarudenko99@gmail.com>
2020-05-19 14:27:18 +0300

    Add change passoword feature

--- src/app/changepassword/changepassword.page.ts
+++ src/app/changepassword/changepassword.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Storage } from '@ionic/storage';
+import { AllService} from '../all.service';
 
 @Component({
   selector: 'app-changepassword',
@@ -6,10 +8,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./changepassword.page.scss'],
 })
 export class ChangepasswordPage implements OnInit {
+  newPass = "";
+  confirmPass = "";
+  userId = "";
 
-  constructor() { }
+  constructor(
+    private allService: AllService,
+    private storage: Storage
+  ) { }
 
   ngOnInit() {
+    this.storage.get('user').then(
+      userinfo => {
+        this.userId = userinfo['user_id'];
+        console.log(userinfo);
+      }
+    )
+  }
+
+  changePassword() {
+    if(this.newPass != this.confirmPass) {
+      console.log(this.newPass.length);
+      this.allService.presentToast("Passwords don't match");
+      return;
+    }
+    if(this.newPass == '') {
+      this.allService.presentToast("Please enter new password");
+      return;
+    }
+    if(this.confirmPass == '') {
+      this.allService.presentToast("Please enter confirm password");
+      return;
+    }
+    let body = new FormData();
+    body.append('user_id', this.userId);
+    body.append('password', this.newPass);
+    this.allService.forgorPassword(body).subscribe(
+      data => {
+        if(data['success'] == 1) {
+          console.log(data);
+          this.allService.presentToast(data['message']);
+        }
+        else {
+          this.allService.presentToast("Error occured. Please try again.");
+        }
+        this.newPass = "";
+        this.confirmPass = "";
+      }
+    )
   }
 
 }
