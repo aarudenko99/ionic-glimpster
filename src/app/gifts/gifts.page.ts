bd266ac4d31aa5509494e1b65e4d1652b8e1ee2b
aarudenko99 <aarudenko99@gmail.com>
2020-05-26 06:13:49 +0300

    Add gift send page

--- src/app/gifts/gifts.page.ts
+++ src/app/gifts/gifts.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Storage } from '@ionic/storage';
+
 import { AllService } from '../all.service';
 
 @Component({
@@ -12,10 +14,18 @@ export class GiftsPage implements OnInit {
   sent = false;
   gifts : any;
   selectedGift = "-1";
+  selectedUser = "-1";
+  // userId = "-1";
   imageBaseUrl = "http://glimpsters.betaplanets.com/MobileApp/uploads/";
+  placeholderImage = "http://1.gravatar.com/avatar/1aedb8d9dc4751e229a335e371db8058?s=96&d=mm&r=g";
+  body = new FormData();
+  otherusers = [];
+  sendTime : any;
+  sendDate : any;
 
   constructor(
     private allService: AllService,
+    private storage: Storage
   ) { }
 
   ngOnInit() {
@@ -26,6 +36,20 @@ export class GiftsPage implements OnInit {
         }
       }
     )
+    
+    this.storage.get('user').then(userInfo => {
+      // this.userId = userInfo.user_id;
+      this.body.append('userId', userInfo.user_id);
+      this.body.append('user_id', userInfo.user_id);
+      this.allService.getUserLocation(this.body).subscribe(
+        data => {
+          if(data['success'] == 1) {
+            this.otherusers = data['users'];
+            console.log('other users--------', data['users']);
+          }
+        }
+      )
+    })
   }
   
 
@@ -44,10 +68,38 @@ export class GiftsPage implements OnInit {
       this.allService.presentToast("Select Gift to Send");
       return;
     }
-      
+    if(this.selectedUser == "-1") {
+      this.allService.presentToast("Select Receiver to Send");
+      return;
+    }
+    if(this.sendDate == undefined) {
+      this.allService.presentToast("Select Date to Send");
+      return;
+    }
+    if(this.sendTime == undefined) {
+      this.allService.presentToast("Select Time to Send");
+      return;
+    }
+    this.body.append('giftId', this.selectedGift);
+    this.body.append('receiverId', this.selectedUser);
+    this.body.append('gift_date', this.sendDate.split("T")[0]);
+    this.body.append('gift_time', this.sendTime.split("T")[1]);
+    this.allService.sendGift(this.body).subscribe(
+      data => {
+        this.allService.presentToast(data['message']);
+        // console.log(data['message']);s
+      }
+    )
+    // console.log(this.sendDate.split("T")[0]);
+    // console.log(this.sendTime.split("T")[1]);
   }
 
   radioChecked(id) {
     this.selectedGift = id;
+  }
+
+  radioChecked1(id) {
+    // console.log(id);
+    this.selectedUser = id;
   }
 }
