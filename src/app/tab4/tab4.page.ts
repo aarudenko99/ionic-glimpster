924d6bdc9af5b6d856fac92c443229f7d925681c
aarudenko99 <aarudenko99@gmail.com>
2020-06-02 10:52:09 +0300

    Fix profile image not showing issue and friend card css

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -20,7 +20,7 @@ export class Tab4Page implements OnInit {
   otherusers = [];
   body = new FormData();
   imageBaseUrl = "http://glimpsters.betaplanets.com/MobileApp/uploads/";
-  placeholderImage = "http://1.gravatar.com/avatar/1aedb8d9dc4751e229a335e371db8058?s=96&d=mm&r=g";
+  placeholderImage = "../../assets/imgs/1.png";
   rooms = [];
   currentId = "";
 
--- src/app/tab5/tab5.page.ts
+++ src/app/tab5/tab5.page.ts
@@ -19,7 +19,7 @@ import { AllService } from '../all.service';
 })
 export class Tab5Page implements OnInit {
   userinfo = [];
-  placeholderImage = "http://1.gravatar.com/avatar/1aedb8d9dc4751e229a335e371db8058?s=96&d=mm&r=g";
+  placeholderImage = "../../assets/imgs/1.png";
   imageBaseUrl = "http://glimpsters.betaplanets.com/MobileApp/uploads/";
   images = [];
   postsCount = 0;
